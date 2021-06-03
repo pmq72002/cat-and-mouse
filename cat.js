79378d44e3be5fad683068b6be70803d1a224279
Minh Quan <pmq7202@gmail.com>
2021-06-03 16:36:37 +0700

    sayHi and chalk for Cat

--- cat.js
+++ cat.js
@@ -1,4 +1,12 @@
-function Cat(){
-
+var chalk = require('chalk');
+function Cat(name){
+this.name = name;
+this.stomach=[];
 }
- module.exports = 'Cat';
+Cat.prototype.eat = function(mouse){
+    this.stomach.push(mouse);
+}
+Cat.prototype.sayHi = function(){
+    console.log("Hi, my name is " + chalk.greenBright(this.name));
+}
+ module.exports = Cat;
